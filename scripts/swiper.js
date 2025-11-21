4e0d3a6da560cb29c9118702e0bd2641a6d01cff
aassaa828-svg <aassaa828@gmail.com>
2025-11-21 18:08:32 +0900

    251121 작업완료

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,8 +1,11 @@
-var swiper = new Swiper(".graphic-section .mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+const swiper = new Swiper(".gallerySwiper", {
+    slidesPerView: 5.5,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    speed: 5000, // 전체 이동 속도 (클수록 느린 흐름)
+    autoplay: {
+        delay: 0, // 멈춤 없이 바로 시작
+        disableOnInteraction: false,
     },
 });
